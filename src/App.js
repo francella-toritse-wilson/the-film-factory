bfabfd90d6180f47385a2e43f7eca9361a47b5d3
wilsonchoi <wilson.choi90@gmail.com>
2023-05-21 13:51:56 -0400

    setup API call, returning results in an array of objects for movie title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
-}
+function App() {
+  const apiKey = `89517ad5b04450b82d2f07f6f3e3d03b`;
+  
+  useEffect(() => {
+    
+    axios({
+      url: 'https://api.themoviedb.org/3/search/movie',
+      method: 'GET',
+      params: {
+        api_key: apiKey,
+        query: 'flkjsdflkj',
+        language: 'en-US',
+        page: '1',
+      }
+      
+      
+    }).then((res)=> {
+      console.log(res.data.results);
+    }).catch((error) => {
+      console.log(error)
+      console.log('Error detected!')
+    })
+
+  })
+
+  return (
+    console.log(`Hello`)
+  )
+
+};
 
 export default App;
+
+
+// error handling, if array returns with 0 indexed items, show something like "no movies found with that title?"
