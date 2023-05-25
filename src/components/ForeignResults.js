26f524df9823bc5a1a445ac4b6149d620d61f3d5
wilsonchoi <wilson.choi90@gmail.com>
2023-05-25 14:34:27 -0400

    commit fix

--- src/components/ForeignResults.js
+++ src/components/ForeignResults.js
@@ -1,10 +1,7 @@
 import { useParams } from "react-router-dom";
-;
 
-const ForeignResults = ({ englishMovie, foreignMovie }) => {
+const ForeignResults = ({ englishMovie }) => {
   const { movieID } = useParams();
-
-  // console.log(foreignMovie[0].original_title);
   return (
     <>
 
@@ -13,8 +10,12 @@ const ForeignResults = ({ englishMovie, foreignMovie }) => {
         <img src={`https://image.tmdb.org/t/p/w200/${englishMovie.poster_path}`}></img>
         <p>{englishMovie.overview}</p>
       </ul>
-        {/* <h2>If you like {englishMovie.original_title}, you will like...{foreignMovie[0].title}</h2> */}
       <button> Save</button>
+
+
+      <ul>
+
+      </ul>
     </>
   );
 };
